f2aacb93f94f5581b3056a3632ae222492b8d43f
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-08-21 00:29:34 +0300

    Обновление

---
+++ server/models/Company.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require('mongoose')
+
+const Company = new Schema({
+
+    companyName: {type:String, unique:true, required:true}, //Название организации
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},      //Владелец
+    img: {type: String},                                    //Картинка организации
+    members: [{type: Schema.Types.ObjectId, ref: 'User'}],  //Участники
+    posts: [{type: Schema.Types.ObjectId, ref: 'Post'}],    //Посты компании
+    companyEmail:{type: String},                            //Email компании
+    companyNumber:{type: String}                            //Номер компании
+})
+
+module.exports = model('Company', Company)
--- server/models/Image.js
+++ server/models/Image.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose')
  
 const Image = new Schema({
-    path: {type: String, required: true}
+    path: {type: String, required: true}    //Путь до картинки
 })
  
 module.exports= model('Image', Image)
