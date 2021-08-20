ce76b6d6da2ebac696f07c3066dbc572bd805c31
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-08-20 18:52:27 +0300

    Создана базовая логика для Auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,19 @@
 const Router = require('express')
 const router = new Router
 const AuthController = require('../controllers/authController')
+const {body} = require('express-validator')
 
+//  http://localhost:5000/contactor/authUser/register
+router.post('/register',
+    body('email').isEmail(),
+    body('password').isLength({min:8, max:24}),
+    AuthController.register 
+)  
 
+//  http://localhost:5000/contactor/authUser/login
+router.post('/login', AuthController.login )    
+
+//  http://localhost:5000/contactor/authUser/activate/:link
+router.get('/activate/:link', AuthController.activate)
 
 module.exports = router
