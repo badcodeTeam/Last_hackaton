8da30c068d0f6d1700c4d9ba49a83e80d88a2def
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-21 01:46:11 +0300

    Update imageController.js

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -26,7 +26,7 @@ class ImgController {
 
     async uploadAvatar(req, res, next) {
 
-        const file = req.body
+        const file = req.files
         console.log(req.files)
         const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
         const decodedToken = jwt.verify(token, process.env.SECRET)
