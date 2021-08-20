dfb2fb1a0f3ba68692ec38b036127c52025055c0
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-08-21 02:06:13 +0300

    update

---
+++ server/services/companyService.js
@@ -0,0 +1,22 @@
+const User = require('../models/User')
+const ApiError = require('../handler/apiError')
+const ClientDto = require('../dtos/clientDto')
+
+class CompanyService {
+
+    async getCompanyInfo(companyId) {
+
+    }
+
+    async addCompany(companyName,) {
+
+    }
+}
+
+module.exports = new CompanyService();
+
+// ----------------------------------------------------------------
+//getAllCompany
+//updateCompany(name)
+
+//Добавить график работы
