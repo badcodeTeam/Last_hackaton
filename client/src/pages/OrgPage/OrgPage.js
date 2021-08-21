9096bd487580bc5ea6822d286f6b68c3ac747b60
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-21 17:27:15 +0300

    QuickFix

--- client/src/pages/OrgPage/OrgPage.js
+++ client/src/pages/OrgPage/OrgPage.js
@@ -38,7 +38,7 @@ const OrgPage = () => {
            <div class="py-10 h-screen w-screen ">
             <div class=" mx-auto shadow-xl  bg-gradient-to-t from-teal-100  to-teal-300 bg-green-200 rounded-lg text-bold overflow-hidden w-3/6 h-full">
                 <div class="flex flex-col justify-center  w-full h-full">
-                    <div class="flex flex-col col-auto my-3 items-center"> <img src={`https://i.imgur.com/S98fN1K.jpg`} width="150px"  class=" rounded-full" />
+                    <div class="flex flex-col col-auto my-3 items-center"> <img src={`http://localhost:5000/contactor/image/company/${orgId}`} width="150px"  class=" rounded-full" />
                         <h1 class="text-xl my-2 text-black font-medium">{!loading && org && org.companyName}</h1> 
                         <span class="text-sm my-2 text-black">Строение {!loading && org && org.building} этаж {!loading && org && org.floor}</span>
                         <span class="text-sm text-black">{!loading && org && <a href={org.site}>Сайт</a>}</span>
@@ -53,21 +53,21 @@ const OrgPage = () => {
                             {!loading && org && userId===org.owner && <input name="field_name" class=" border border-2 rounded-r px-4 py-2 w-full " type="text" placeholder="Новая запись" />}
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/company/${orgId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">{!loading && org && org.companyName}</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
                             </div>
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/company/${orgId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">{!loading && org && org.companyName}</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
                             </div>
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/company/${orgId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">{!loading && org && org.companyName}</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
