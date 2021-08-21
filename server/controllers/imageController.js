9dc66390eb000bb4747040c9983426aae9b00fe0
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-08-21 17:37:55 +0300

    Update imageController.js

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -54,9 +54,8 @@ class ImgController {
         return res.json(saveAvatar)
     }
 
-    //  http://localhost:5000/contactor/image/company/:id 
+    //  http://localhost:5000/contactor/image/company/:id
     async getCompanyImage(req, res, next){
- 
         const companyId = req.params.id;
         const company = await Company.findById(companyId)
         if(!user){
