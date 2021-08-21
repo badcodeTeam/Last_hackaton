a842a05503f87d3659991921b6b2c54f59fc2ecf
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-21 23:43:19 +0300

    Event Update

--- client/src/pages/EventsPage/EventsPage.jsx
+++ client/src/pages/EventsPage/EventsPage.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, {useState, useEffect, useCallback, useContext} from "react";
+import {useHttp} from "../../utils"
+import {AuthContext} from "../../utils/context/Auth.context"
+import {EventCard} from './EventCard'
 
 const EventPage = () => {
+  const [orgs, setOrgs] = useState([])
+  const {loading, request} = useHttp()
+  
+
+  const getOrgs = useCallback(async () => {
+    try{
+      const data = await request('http://localhost:5000/contactor/company/getAllCompanies', 'get', null, {})
+      console.log(data.getAllCompanies)
+      setOrgs(data.getAllCompanies)
+    }catch(e){
+      console.log(e)
+    }
+  },[request])
+
+  useEffect(()=>{
+    getOrgs()
+  }, [getOrgs])
+
+
+
   return (
-    <div className="w-full flex-grow p-10 flex flex-col items-center">
-      <h1 className="text-3xl mb-3">Будущие мероприятия: </h1>
-      <div className="w-full flex-grow flex flex-wrap overflow-auto hide-scrollbar">
-        <div className="w-2/4 h-2/4 bg-black border-8 border-white"></div>
-        <div className="w-2/4 h-2/4 bg-black border-8 border-white"></div>
-        <div className="w-2/4 h-2/4 bg-black border-8 border-white"></div>
-        <div className="w-2/4 h-2/4 bg-black border-8 border-white"></div>
-      </div>
-    </div>
+    <ul className="w-full h-full flex-grow hide-scrollbar flex flex-wrap overflow-auto">
+      { /*
+      <ResidentCard
+        residentPreviewImage="https://images.unsplash.com/photo-1608434934019-f8dd8d7e8cae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+        residentBuilding={1}
+        residentFloor={1}
+        residentName="ImpreStudio"
+        residentDescription="Art gallery"
+        residentTimeStart="8:00"
+        residentTimeEnd="18:00"
+      />
+      */}
+
+      {!loading && orgs && orgs.map((org, index) => {
+        return (
+          <EventCard
+            key={index}
+            residentPreviewImage="https://images.unsplash.com/photo-1608434934019-f8dd8d7e8cae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+            residentBuilding={org.building}
+            residentFloor={org.floor}
+            residentName={org.companyName}
+            residentDescription={org.direction}
+            residentTimeStart={org.scheduleStart}
+            residentTimeEnd={org.scheduleEnd}
+            residentSiteUrl={org.site}
+            residentId ={org._id}
+          />
+        )
+      })}
+    </ul>
   );
 };
 
