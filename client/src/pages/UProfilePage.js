7cf28eb0d9290cc2462293a615fee0bf2e22477e
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-21 11:26:37 +0300

    Update UProfilePage.js

--- client/src/pages/UProfilePage.js
+++ client/src/pages/UProfilePage.js
@@ -35,9 +35,9 @@ const UProfilePage = () => {
            <div class="py-10 h-screen w-screen ">
             <div class=" mx-auto shadow-xl  bg-gradient-to-t from-teal-100  to-teal-300 bg-green-200 rounded-lg text-bold overflow-hidden w-3/6 h-full">
                 <div class="flex flex-col justify-center  w-full h-full">
-                    <div class="flex flex-col col-auto my-3 items-center"> <img src={`http://localhost:5000/contactor/image/user/${usrId}`} width="100px" height="20px" class="h-2/5 rounded-full" />
+                    <div class="flex flex-col col-auto my-3 items-center"> <img src={`http://localhost:5000/contactor/image/user/${usrId}`} width="150px"  class=" rounded-full" />
                         <h1 class="text-xl my-2 text-black font-medium">{!loading && user && user.name}</h1> 
-                        <span class="text-sm text-black">Создатель File Sharing Team</span>
+                        <span class="text-sm text-black">Создатель .badcode</span>
                         <span class="text-sm my-2 text-black">{!loading && user && user.number}</span>
                     </div>
                     <div class="flex flex-col col-auto my-3 items-center">
@@ -49,21 +49,21 @@ const UProfilePage = () => {
                             {!loading && user && userId===usrId && <input name="field_name" class=" border border-2 rounded-r px-4 py-2 w-full " type="text" placeholder="Новая запись" />}
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/user/${usrId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">Дмитриев Максим Сергеевич</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
                             </div>
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/user/${usrId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">Дмитриев Максим Сергеевич</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
                             </div>
                         </div>
                         <div className="flex flex-row my-5 h-2/6 w-5/6 bg-white rounded-lg shadow-md">
-                            <img src="https://i.imgur.com/S98fN1K.jpg"   class="my-auto mx-5 rounded-full h-3/6" />
+                            <img src={`http://localhost:5000/contactor/image/user/${usrId}`}   class="my-auto mx-5 rounded-full h-3/6" />
                             <div className="flex flex-col my-auto">
                                 <h5 class="text-md  text-black font-medium">Дмитриев Максим Сергеевич</h5> 
                                 <span class="text-sm text-black">Аннонс мероприятия по работе в сфере IT. Более детальная информация появится на странице организации.</span>
