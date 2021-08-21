2d8f5d3b9e16e8c879bb7228f1f021cc00239dfc
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-21 03:00:44 +0300

    Navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, {useContext} from "react";
 import NotificationBar from "../NotificationBar/NotificationBar.jsx";
 import "./Navbar.css";
 import NavButton from "./NavButton.jsx";
 import { Link } from "react-router-dom";
+import {AuthContext} from "../../utils/context/Auth.context"
 
 const Navbar = () => {
+  const {userId} = useContext(AuthContext)
+  const profileLink = "/profile/" + userId
   return (
     <nav className="w-full border navbar p-5">
       <div className="flex items-center justify-evenly w-full h-full">
-        <div>
-          <p className="text-left text-2xl text-green-400">Контактор</p>
-          <p className="text-xs">платформа управления</p>
-        </div>
+        <Link to="/">
+          <div>
+            <p className="text-left text-2xl text-green-400">Контактор</p>
+            <p className="text-xs">платформа управления</p>
+          </div>
+        </Link>
         <NavButton>Главная</NavButton>
-        <NavButton>Личный кабинет</NavButton>
+        <Link to={profileLink}> <NavButton>Личный кабинет</NavButton></Link>
         <NavButton>Мероприятия</NavButton>
-        <Link to="/residents">
-          <NavButton>Резиденты</NavButton>
-        </Link>
-        <Link to="/contact_us">
-          <NavButton>Контакты</NavButton>
-        </Link>
+        <NavButton>Резиденты</NavButton>
+        <Link to="/contact_us"><NavButton>Контакты</NavButton></Link>
         <NotificationBar />
       </div>
     </nav>
